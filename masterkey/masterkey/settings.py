0c247fbf2676ff47c37c10e0892c2d2448c024e0
Muhammad <samsmusa@outlook.com>
2022-09-08 03:22:46 +0600

    db fix

--- masterkey/masterkey/settings.py
+++ masterkey/masterkey/settings.py
@@ -126,8 +126,7 @@ WSGI_APPLICATION = 'masterkey.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.' + config('DB_ENGINE', 'mysql'),
-        'NAME': 'masterkey',
-        # 'NAME': config('DB_NAME', 'masterkey'),
+        'NAME': config('DB_NAME', 'masterkey'),
         'USER': config('DB_USERNAME', 'root'),
         'PASSWORD': config('DB_PASSWORD', 'root'),
         'HOST': config('DB_HOST', 'localhost'),
